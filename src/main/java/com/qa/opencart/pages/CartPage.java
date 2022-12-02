77951821d906e688492d0974e1e89aca088c3420
sudha <sudhaveerabathran@gmail.com>
2022-12-01 22:05:14 -0500

    added as per review comments by Navi

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,7 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	By cart=By.id("cart");
+	
+	
+	public boolean addcart() {
+		System.out.println("Add things");
+		return true;
+	}
+	
 	public static void main(String[] args) {
 	System.out.println("add to cart");
 	}
